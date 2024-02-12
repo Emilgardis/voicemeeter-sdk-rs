842d92803bb83be3e873e9b470597c1718610a29
Emilgardis <Emilgardis@users.noreply.github.com>
2024-02-12 19:31:56 +0000

    deploy: c21207077708aae252dc9ec7118db46bbc58b7aa

--- src-files.js
+++ src-files.js
@@ -1,4 +1,4 @@
 var srcIndex = new Map(JSON.parse('[\
-["voicemeeter",["",[["interface",[["callback",[["data",[],["buffer_abstraction.rs"]]],["commands.rs","data.rs","register.rs","start_stop.rs"]],["parameters",[],["bus.rs","eq.rs","errors.rs","get_parameters.rs","option.rs","set_parameters.rs","strip.rs"]]],["callback.rs","communication_login_logout.rs","device.rs","general_information.rs","get_levels.rs","macro_buttons.rs","parameters.rs"]]],["bindings.rs","interface.rs","lib.rs","types.rs"]]]\
+["voicemeeter",["",[["interface",[["callback",[["data",[],["buffer_abstraction.rs"]]],["commands.rs","data.rs","register.rs","start_stop.rs"]],["parameters",[],["bus.rs","eq.rs","errors.rs","fx.rs","get_parameters.rs","option.rs","recorder.rs","set_parameters.rs","strip.rs","vban.rs"]]],["callback.rs","communication_login_logout.rs","device.rs","general_information.rs","get_levels.rs","macro_buttons.rs","parameters.rs"]]],["bindings.rs","interface.rs","lib.rs","types.rs"]]]\
 ]'));
 createSrcSidebar();
